49eafcea1f9f4db6485d1388a11c9124ffefcf7a
Antonio <antoarcejr@gmail.com>
2022-11-05 19:54:26 -0400

    Spacing

--- hackathon/src/router/index.js
+++ hackathon/src/router/index.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Homepage from "@/components/Homepage";
 import About from "@/components/About.vue";
 
+
 Vue.use(Router);
 export default new Router({
   routes: [
@@ -11,6 +12,11 @@ export default new Router({
       name: "Homepage",
       component: Homepage
     },
-    { path: "/about", name: "About", component: About }
+    { 
+      path: "/about", 
+      name: "About", 
+      component: About 
+    }
+
   ]
 });
