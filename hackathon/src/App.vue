bf6bd8b0f1322ef199de7d8b55cb5c3412ddba63
Jaime Nufio <jaimeenufio@gmail.com>
2022-11-05 13:02:08 -0400

    navbar and bootstrap fixes

--- hackathon/src/App.vue
+++ hackathon/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-dark navbar-expand-lg bg-dark mb-5">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
