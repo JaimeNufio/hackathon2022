bd6f25f8fdce68f8989351dd3893aa0caacb9638
Jaime Nufio <jaimeenufio@gmail.com>
2022-11-05 12:42:07 -0400

    drop padding top on app.vue

--- hackathon/src/App.vue
+++ hackathon/src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <router-view/>
   </div>
 </template>
@@ -18,7 +42,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 @import '~bootstrap/dist/css/bootstrap.css'
 </style>
