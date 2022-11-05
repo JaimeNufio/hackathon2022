ca2b0782c5b2de9c9f599eca05b87d2c8ab05978
Antonio <antoarcejr@gmail.com>
2022-11-05 14:20:05 -0400

    Clean up Navbar

--- hackathon/src/App.vue
+++ hackathon/src/App.vue
@@ -3,7 +3,7 @@
     <!-- <img src="./assets/logo.png"> -->
     <nav class="navbar navbar-dark navbar-expand-lg bg-dark mb-5">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Fyrestarter</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -16,10 +16,10 @@
           <a class="nav-link" href="#">Campers Checklist</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
+          <a class="nav-link" href="#">Nearby Campsites</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href="#">About Us</a>
         </li>
       </ul>
     </div>
