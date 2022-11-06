848b9c14507d085891ce3b076a1ecbb89c70758f
Antonio <antoarcejr@gmail.com>
2022-11-06 00:02:50 -0400

    Add checklist to navbar

--- hackathon/src/router/index.js
+++ hackathon/src/router/index.js
@@ -4,6 +4,8 @@ import Router from "vue-router";
 import Homepage from "@/components/Homepage";
 import About from "@/components/About.vue";
 import Browse from '@/components/Browse.vue'
+import Checklist from '@/components/Checklist.vue'
+
 
 Vue.use(Router)
 
@@ -23,10 +25,10 @@ export default new Router({
       name: "Homepage",
       component: Homepage
     },
-    { 
-      path: "/about", 
-      name: "About", 
-      component: About 
+    {
+      path: '/checklist',
+      name: "Checklist",
+      component: Checklist
     }
 
   ]
