78aba74998a883e5bcdea94fe89750fe2fb9dea7
Jaime Nufio <jaimeenufio@gmail.com>
2022-11-06 07:08:35 -0500

    A E S T H E T I C S

--- hackathon/src/components/Homepage.vue
+++ hackathon/src/components/Homepage.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="container mt-5" style="height:40vh">
+  <div>
+  <div class="p-5" style="height:50vh; ">
     <div style="margin-bottom:15vh">
     <h1 style="text-align:left; font-size: 3em;">FyreStartr</h1>
     <h2 style="text-align:left; font-size: 1.2em;">Your first stop towards making your camping trip awesome.</h2>
@@ -7,17 +8,75 @@
     <div class="">
       <div class="row align-items-md-stretch" style="margin-top:5vh">
         <div class="col-md-6"></div>
-        <div class="col-md-6">
-          <div class="h-100 p-3 text-bg-dark rounded-3 d-flex">
-            <div class="flex-1" s></div>
+        <div class="col-md-6 pr-5">
+          <div class=" p-5 text-bg-dark rounded-3 d-flex ">
+            <div class="flex-1"></div>
             <!-- <img src="../assets/campfire.jpg" alt="Campfire" style="width:350px" > -->
-           <button href="/survey" class="btn btn-outline-light float-right" style="font-size:1.2em;  margin-left: auto; order: 2;" type="button">
+           <button href="/survey" class="btn btn-outline-light" style="font-size:1.2em;  margin-left: auto; order: 2;" type="button">
              Let's Get Started
             </button>
           </div>
         </div>
-
       </div>
+    </div>
+    </div>
+    <div class="copy-parent" style="height:60vh;width: 100%;background-color: black;">
+      <div class="p-5">
+        <div class="d-flex">
+          <div class="p-2"><img style=" max-width:400px;border-radius: 25px;" src="../assets/starycampfire.jpeg"/></div>
+          <div class="p-3" style="flex:1; text-align: left; position: relative;">
+            <div>
+              <div style="font-size:1.5em">Find your ideal getaway faster.</div>
+              <div style="font-size:1em">So you can start enjoying every monment sooner.</div>
+            </div>
+            <div style="font-size:1em;flex:1;  position: absolute; bottom: 0; left: 20px;">Get started with our easy camp ground browser ground browser.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copy-parent" style="height:60vh;width: 100%;background: linear-gradient(49deg, rgba(236,155,8,1) 0%, rgba(212,9,9,1) 53%, rgba(0,0,0,1) 100%);">
+      <div class="p-5 ">
+        <div class="d-flex">
+          <div class="p-3" style="flex:1; text-align: left; position: relative;">
+            <div>
+              <div style="font-size:1.5em">Spend less time worrying about forgetting something important.</div>
+              <div style="font-size:1em">We'll help you make a list of things so you won't forget something you need.</div>
+            </div>
+            <div style="font-size:1em;flex:1;  position: absolute; bottom: 0; left: 20px;">Our recommendation engine takes into consideration many factors.</div>
+          </div>
+          <div class="p-2"><img style=" max-width:400px;border-radius: 25px;" src="../assets/think.jpeg"/></div>
+        </div>
+      </div>
+    </div>
+    <div class="copy-parent" style="height:60vh;width: 100%;background-color: black;" >
+      <div class="p-5 ">
+        <div class="d-flex">
+          <div class="p-2"><img style=" max-width:400px;border-radius: 25px;" src="../assets/autumn.jpeg"/></div>
+          <div class="p-3" style="flex:1; text-align: left; position: relative;">
+            <div>
+              <div style="font-size:1.5em">Enjoy the season while its still here.</div>
+              <div style="font-size:1em">Take advantage of these tools to leverage them into adventure.</div>
+            </div>
+            <div style="font-size:1em;flex:1;  position: absolute; bottom: 0; left: 20px;">There is no excuse to not take that trip you've been thinking about.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copy-parent" style="height:60vh;width: 100%;background: rgb(240,79,79);background: linear-gradient(149deg, rgba(240,79,79,1) 0%, rgba(97,36,108,1) 55%, rgba(0,0,0,1) 100%);">
+      <div class="p-5 ">
+        <div class="d-flex">
+                  <div class="p-3" style=" text-align: left; position: relative;">
+            <div>
+              <div style="font-size:1.5em">Start seeing the world.</div>
+              <div style="font-size:1em">You've only got one life. Why waste any moment?</div>
+            </div>
+            <div style="font-size:1em;flex:1;  position: absolute; bottom: 0; left: 20px;">Starting planning your next adventure today.</div>
+          </div>
+          <div class="p-2"><img style="flex:1; max-width:400px;border-radius: 25px;" src="../assets/steamhams.jpeg"/></div>
+
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -27,9 +86,6 @@ export default {
   name: 'Homepage',
   data () {
     return {
-      msg: 'Start your camping experience with Fyrestart',
-      navy: `What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.`,
-      lorem: 'Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.'
     }
   },
   created () {
@@ -58,6 +114,12 @@ li {
 .roundEdge {
   border-radius: 25px;
   padding: 20px;
+}
+
+.copy-parent{
+  display: flex !important;
+  justify-content: center !important;
+  align-items: center !important;
 }
 
 </style>
